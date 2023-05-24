d18c74c134a21f455d9fbc8dadac0ead059638fc
Oscar-Skinner <1027514@student.sae.edu.au>
2023-05-24 11:05:39 +1000

    vision cone prefab and dynamic object is a serialized monobehaviour now (if you want something else ask and ill change)

--- Assets/Member Test Folders/OscarTest/AI/Vision/DynamicObject.cs
+++ Assets/Member Test Folders/OscarTest/AI/Vision/DynamicObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class DynamicObject : MonoBehaviour
+public class DynamicObject : SerializedMonoBehaviour
 {
     public bool isAI;
     public bool isPlayer;
